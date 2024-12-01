7da9aeaf531729b948229f002cf233ad81d734f1
[Damien Wallace Eagles] <damien.eagles@gmail.com>
2024-12-02 04:56:06 +1000

    Update publish_dacpac.bat
    Leave out local path

--- DoctorsWebApplication/publish_dacpac.bat
+++ DoctorsWebApplication/publish_dacpac.bat
@@ -1,4 +1,4 @@
 ECHO Copying DACPAC...
-cd C:\[Solutions]\[2023DoctorsWebApplication]\DoctorsWebApplication
+REM cd C:\[Solutions]\[2023DoctorsWebApplication]\DoctorsWebApplication
 xcopy /e /y /-I DoctorsDatabase\bin\release\DoctorsDatabase.dacpac DoctorsWebApplication\bin\Release\net9.0\publish\DoctorsDatabase.dacpac
 
